8e39248c072d9367c8471492800fd288cc01296b
SID THOKAL <sidthokal@gmail.com>
2022-05-26 18:51:00 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,31 @@
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import "dotenv/config";
+
+import postRoutes from './routes/posts.js';
+import userRouter from "./routes/user.js";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.get('/',(req,res) => {
+  res.send('Hello and Welcome to Algorithmus API');
+});
+
+app.use('/posts', postRoutes);
+app.use("/user", userRouter);
+
+const CONNECTION_URL = process.env.MONGO_URL;
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
